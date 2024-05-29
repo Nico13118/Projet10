a33a720c77e248c50d4c79bf9fc323cef9575375
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-05-29 10:31:47 +0200

    Configuration de l'interface d'administration afin de personnaliser l'affichage des utilisateurs.

--- softdesk/admin.py
+++ softdesk/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username', 'email', 'age',
+        'can_be_contacted', 'can_data_be_shared',
+        'is_active', 'is_staff', 'is_superuser')
+
+
+admin.site.register(User, UserAdmin)
