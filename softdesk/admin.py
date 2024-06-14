bd00fb09eeb1cf4fdcfeabbda047288a32301d85
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-06-14 17:42:55 +0200

    Reconfiguration du fichier admin afin d'avoir l'affichage des projets et contributeurs dans la page d'administration django.

--- softdesk/admin.py
+++ softdesk/admin.py
@@ -1,12 +1,34 @@
 from django.contrib import admin
-from .models import User
+from softdesk.models import User, Project, Contributor
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = (
-        'username', 'email', 'age',
+        'id', 'username', 'email', 'age',
         'can_be_contacted', 'can_data_be_shared',
-        'is_active', 'is_staff', 'is_superuser')
+        'is_active', 'is_staff', 'is_superuser'
+    )
+    ordering = ('id',)
 
 
-admin.site.register(User, UserAdmin)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'created_time', 'author', 'get_contributors',
+        'project_name', 'project_description', 'type'
+    )
+    ordering = ('id',)
+
+    def get_contributors(self, obj):
+        return ", ".join([contributor.user.username for contributor in obj.contributors.all()])
+    get_contributors.short_description = 'Contributors'
+
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'date_joined', 'user', 'project'
+    )
+    ordering = ('id',)
+
