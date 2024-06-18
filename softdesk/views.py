32dffddbb5de6e384707cc60ccc5d72318a85739
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-06-18 11:10:56 +0200

    Configuration de la classe RegisterUserViewset et ajout d'une fonction get_permission, un utilisateur lambda ne peut que s'inscrire, les autres actions sont reservées aux superusers

--- softdesk/views.py
+++ softdesk/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
+from softdesk.models import User
+from softdesk.permissions import IsUserAuthenticated, IsSuperUser
+from softdesk.serializers import RegisterUserSerializer
 
-# Create your views here.
+
+# ModelViewSet : create()`, `retrieve()`, `update()`, `partial_update()`, `destroy()`, `list()`
+
+class RegisterUserViewset(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegisterUserSerializer
+
+    def get_permissions(self):
+        if self.action in ['create']:
+            self.permission_classes = [AllowAny]
+
+        else:
+            self.permission_classes = [IsSuperUser]
+        return super().get_permissions()
+
+
