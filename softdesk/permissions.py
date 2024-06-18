6b82e130de5c1b5713e20cf06e1edb9b5061d88e
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-06-18 10:56:31 +0200

    Configuration d'une permision concernant le superuser

--- softdesk/permissions.py
+++ softdesk/permissions.py
@@ -5,3 +5,8 @@ class IsUserAuthenticated(BasePermission):
 
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_authenticated)
+
+
+class IsSuperUser(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user.is_superuser)
