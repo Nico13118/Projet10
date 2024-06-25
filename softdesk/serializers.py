dbdc867e92a73dc4179996d2e5a942d8347093cd
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-06-25 10:42:46 +0200

    Configuratio du serializer concernant la classe Project et Contributor

--- softdesk/serializers.py
+++ softdesk/serializers.py
@@ -1,9 +1,10 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, ValidationError
 
-from .models import User
+from .models import User, Project, Contributor
 
 
-class RegisterUserSerializer(ModelSerializer):
+class RegisterUserListSerializer(ModelSerializer):
 
     class Meta:
         model = User
@@ -19,3 +20,19 @@ class RegisterUserSerializer(ModelSerializer):
         return value
 
 
+class ProjectListSerializer(ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = [
+            'id', 'created_time', 'author', 'project_name', 'project_description', 'type'
+        ]
+
+
+class ContributorSerializer(ModelSerializer):
+
+    class Meta:
+        model = Contributor
+        fields = [
+            'id', 'date_joined', 'user', 'project',
+        ]
