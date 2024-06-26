23cef584e22482c2a070cd29ed583f47ffac38fe
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-06-26 14:25:09 +0200

    Configuration de la classe ProjectViewset avec la fonction get_permission qui donne l'autorisation à un utilisateur authentifié de créer un projet, les autres actions sont utilisables que pour un super user. La fonction perform_create permet d'ajouter le nom de l'utilisateur dans le champs author, On procède à a création de l'objet pour la classe Contributor en ajoutant le nom d'utilisateur dans le champs user et le projet dans le champs projet. Configuration de la classe ContributorViewset avec les permissions.

--- softdesk/views.py
+++ softdesk/views.py
@@ -1,15 +1,15 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
-from softdesk.models import User
+from softdesk.models import User, Project, Contributor
 from softdesk.permissions import IsUserAuthenticated, IsSuperUser
-from softdesk.serializers import RegisterUserSerializer
+from softdesk.serializers import RegisterUserListSerializer, ProjectListSerializer, ContributorSerializer
 
 
 # ModelViewSet : create()`, `retrieve()`, `update()`, `partial_update()`, `destroy()`, `list()`
 
 class RegisterUserViewset(ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = RegisterUserSerializer
+    serializer_class = RegisterUserListSerializer
 
     def get_permissions(self):
         if self.action in ['create']:
@@ -19,4 +19,32 @@ class RegisterUserViewset(ModelViewSet):
             self.permission_classes = [IsSuperUser]
         return super().get_permissions()
 
+
+class ProjectViewset(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectListSerializer
+
+    def get_permissions(self):
+        if self.action in ['create']:
+            self.permission_classes = [IsUserAuthenticated]
+        else:
+            self.permission_classes = [IsSuperUser]
+
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        project = serializer.save(author=self.request.user)
+        Contributor.objects.create(user=self.request.user, project=project)
+
+
+class ContributorViewset(ModelViewSet):
+    queryset = Contributor.objects.all()
+    serializer_class = ContributorSerializer
+
+    def get_permissions(self):
+        if self.action in ['create']:
+            self.permission_classes = [IsUserAuthenticated]
+        else:
+            self.permission_classes = [IsSuperUser]
+        return super().get_permissions()
 
