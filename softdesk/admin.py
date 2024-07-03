6444d44db270c912edaa31fb7f4867a63557b8e5
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-07-03 17:39:16 +0200

    Mise à jour du fichier admin.py

--- softdesk/admin.py
+++ softdesk/admin.py
@@ -15,14 +15,10 @@ class UserAdmin(admin.ModelAdmin):
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'created_time', 'author', 'get_contributors',
-        'project_name', 'project_description', 'type'
+        'id', 'created_time', 'author', 'project_name',
+        'project_description', 'type'
     )
     ordering = ('id',)
-
-    def get_contributors(self, obj):
-        return ", ".join([contributor.user.username for contributor in obj.contributors.all()])
-    get_contributors.short_description = 'Contributors'
 
 
 @admin.register(Contributor)
