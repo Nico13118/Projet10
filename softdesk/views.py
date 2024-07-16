49f48792ee0139d3209deb644db6c1bda8efb008
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-07-16 10:43:09 +0200

    Configuration de la fonction perform_create, cette fonction effectue deux contrôles pour vérifier que l'utilisateur connecté soit bien l'auteur du projet et que l'utilisateur selectionné dans le formulaire ne soit pas déjà contributeur de ce projet. Si l'un de ces contrôles échoue, une ValidationError est levé sinon il enregistre le contributeur à un projet

--- softdesk/views.py
+++ softdesk/views.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 from softdesk.models import User, Project, Contributor
@@ -10,7 +11,6 @@ from softdesk.serializers import RegisterUserListSerializer, ProjectListSerializ
 
 
 class MultipleSerializerMixin(ModelViewSet):
-
     detail_serializer_class = None
 
     def get_serializer_class(self):
@@ -56,9 +56,35 @@ class ContributorViewset(MultipleSerializerMixin, ModelViewSet):
     detail_serializer_class = ContributorDetailSerializer
 
     def get_permissions(self):
-        if self.action in ['list', 'retrieve']:
+        if self.action in ['create', 'list', 'retrieve']:
             self.permission_classes = [IsUserAuthenticated]
         else:
             self.permission_classes = [IsSuperUser]
         return super().get_permissions()
 
+    def perform_create(self, serializer):
+        """
+        Avant l'enregistrement d'un contributeur, deux contrôles sont effectués :
+        1) Vérifie que l'utilisateur connecté soit bien l'auteur du projet.
+        2) Vérifie que l'utilisateur selectionné dans le formulaire ne soit pas déjà contributeur du projet.
+
+        Si l'un de ces contrôles échoue, une ValildationError est levée.
+        """
+        get_user_id_auth = self.request.user.id  # Récuperation de l'id de l'utilisateur connecté
+        get_project_id_form = self.request.data.get('project')  # Récuperation de (project_id) depuis le formulaire
+        get_project_name = Project.objects.get(id=get_project_id_form)  # Récupère le nom du projet au format str
+        exist_project_name_author = Project.objects.filter(author=get_user_id_auth,
+                                                           project_name=get_project_name).exists()
+        """ Si l'utiliateur connecté est bien l'auteur du projet selectionné dans le formulaire."""
+        if exist_project_name_author:
+            get_user_id_form = self.request.data.get('user')  # Récupération du (user_id) depuis le formulaire
+            user_name = User.objects.get(id=get_user_id_form)  # Récupérer le nom du (user) avec son id.
+            exist_contributor_in_project = self.queryset.filter(user=user_name,
+                                                                project=get_project_name).exists()
+            """ Si l'utilisateur selectionné dans le formulaire est déjà contributeur de ce projet."""
+            if exist_contributor_in_project:
+                raise ValidationError("The chosen user is already a contributor to this project.")
+            else:
+                serializer.save(user=user_name, project=get_project_name)
+        else:
+            raise ValidationError("You are not allowed to add a contributor for this project.")
