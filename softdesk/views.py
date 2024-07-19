ee16c49e2ce90e3b396202b5a33e55dbb30b4ec4
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-07-19 18:01:44 +0200

    Plusieurs modifications dans les viewset afin qu'ils puissent utiliser la même fonction get_objects_all_models. Quelques modifications au niveau des commentaires.

--- softdesk/views.py
+++ softdesk/views.py
@@ -37,7 +37,7 @@ class MultipleSerializerMixin(ModelViewSet):
 
 
 class RegisterUserViewset(ModelViewSet):
-    queryset = User.objects.all()
+    queryset = GetObjects.get_objects_all_models(User)
     serializer_class = RegisterUserListSerializer
 
     def get_permissions(self):
@@ -87,22 +87,24 @@ class ContributorViewset(MultipleSerializerMixin, ModelViewSet):
 
         Si l'un de ces contrôles échoue, une ValildationError est levée.
         """
-        get_user_id_auth = self.request.user.id  # Récuperation de l'id de l'utilisateur connecté
-        get_project_id_form = self.request.data.get('project')  # Récuperation de (project_id) depuis le formulaire
-        get_project_name = Project.objects.get(id=get_project_id_form)  # Récupère le nom du projet au format str
-        exist_project_name_author = Project.objects.filter(author=get_user_id_auth,
-                                                           project_name=get_project_name).exists()
-        """ Si l'utiliateur connecté est bien l'auteur du projet selectionné dans le formulaire."""
+        obj_user_auth = self.request.user
+        obj_project = GetObjects.get_objects_all_models(Project, self.request.data.get('project'))
+        """ 
+        Recherche que l'utilisateur connecté est bien l'auteur du projet selectionné dans le formulaire et 
+        retourne True ou False.
+        """
+        exist_project_name_author = Project.objects.filter(author=obj_user_auth,
+                                                           project_name=obj_project).exists()
         if exist_project_name_author:
-            get_user_id_form = self.request.data.get('user')  # Récupération du (user_id) depuis le formulaire
-            user_name = User.objects.get(id=get_user_id_form)  # Récupérer le nom du (user) avec son id.
-            exist_contributor_in_project = self.queryset.filter(user=user_name,
-                                                                project=get_project_name).exists()
-            """ Si l'utilisateur selectionné dans le formulaire est déjà contributeur de ce projet."""
+            obj_user_form = GetObjects.get_objects_all_models(User, self.request.data.get('user'))
+
+            """ Recherche que l'utilisateur selectionné dans le formulaire est déjà contributeur de ce projet."""
+            exist_contributor_in_project = self.queryset.filter(user=obj_user_form,
+                                                                project=obj_project).exists()
             if exist_contributor_in_project:
                 raise ValidationError("The chosen user is already a contributor to this project.")
             else:
-                serializer.save(user=user_name, project=get_project_name)
+                serializer.save(user=obj_user_form, project=obj_project)
         else:
             raise ValidationError("You are not allowed to add a contributor for this project.")
 
@@ -113,7 +115,6 @@ class IssueViewset(ModelViewSet):
 
     def get_permissions(self):
         if self.action in ['create', 'list']:
-
             self.permission_classes = [IsUserAuthenticated]
         else:
             self.permission_classes = [IsSuperUser]
@@ -123,22 +124,25 @@ class IssueViewset(ModelViewSet):
         """
         Avant l'enregistrement de l'issue, deux contrôles sont effectués : :
         1) Vérifier que l'utilisateur connecté soit bien contributeur du projet selectionné dans le formulaire.
-        2) Vérifier que l'utilisateur sélectionné dans le formulaire est contributeur du projet
+        2) Vérifier que l'utilisateur sélectionné dans le formulaire est contributeur du projet.
+
+        Si l'un de ces contrôles échoue, une ValildationError est levée.
         """
         """ Récupérer l'objet de l'utilisateur connecté """
         obj_user_auth = self.request.user
         """ Récupérer l'objet du projet en provenance du formulaire"""
         obj_project = GetObjects.get_objects_all_models(Project, self.request.data.get('project'))
 
+        """ Recherche que l'utilisateur connecté est bien contributeur du projet et retourne True ou False """
         exist_contributor_in_project = Contributor.objects.filter(user=obj_user_auth,
                                                                   project=obj_project).exists()
-        """ Si l'utilisateur connecté est bien contributeur du projet et retourne True ou False """
         if exist_contributor_in_project:
-            """ Récupérer l'objet de l'utilisateur assigné à l'Issue"""
+            """ Récupérer l'objet utilisateur assigné à l'Issue"""
             obj_user_form = GetObjects.get_objects_all_models(User, self.request.data.get('assigned_contributor'))
+
+            """ Recherche que l'utilisateur selectionné dans le formulaire est bien contributeur du projet"""
             exist_contributor_in_project2 = Contributor.objects.filter(user=obj_user_form,
                                                                        project=obj_project).exists()
-            """ Si l'utilisateur selectionné dans le formulaire est bien contributeur du projet"""
             if exist_contributor_in_project2:
                 serializer.save(author=obj_user_auth,
                                 assigned_contributor=obj_user_form,
