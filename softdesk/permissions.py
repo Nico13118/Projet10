a19c22d7793f98f26d191775452954a2320627e7
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-07-22 17:18:18 +0200

    Création de la classe IsContributor avec la fonctio has_oject_permission qui permet de contrôler si l'utilisateur est bien contributeur du projet

--- softdesk/permissions.py
+++ softdesk/permissions.py
@@ -10,3 +10,15 @@ class IsUserAuthenticated(BasePermission):
 class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
         return bool(request.user.is_superuser)
+
+
+class IsContributor(BasePermission):
+    message = None
+
+    def has_object_permission(self, request, view, obj):
+        list_contributors = obj.contributors.all()
+        info_contrib = [c for c in list_contributors if c == request.user]
+        if not info_contrib:
+            self.message = "You must be a contributor to this project to be able to access the details."
+        return bool(info_contrib)
+
