8007b415d14bb5857a073279ddb16ab20f7c0c53
Nicolas Sylvestre <sylvestrenicolas@sfr.fr>
2024-07-25 18:05:49 +0200

    Déplacement du modèle User de l'application softdesk vers authentication

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    date_joined = models.DateTimeField(auto_now_add=True)
+    username = models.CharField(max_length=150, unique=True)
+    age = models.IntegerField(blank=False, default=0)
+    password = models.CharField(max_length=150, blank=False)
+    can_be_contacted = models.BooleanField(default=False)
+    can_data_be_shared = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
